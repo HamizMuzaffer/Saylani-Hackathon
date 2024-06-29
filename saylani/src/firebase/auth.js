10820413cd2185ced9e127fab1260055c488b5d7
HamizMuzaffer <hamiz.muzaffer77@gmail.com>
2024-06-29 14:21:16 -0700

    redux added

---
+++ saylani/src/firebase/auth.js
@@ -0,0 +1,78 @@
+import { auth } from "../config/Config";
+
+
+import {
+    getAuth,
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword,
+    GoogleAuthProvider,
+    signInWithPopup,
+    GithubAuthProvider,
+    onAuthStateChanged,
+    signOut,
+    updateProfile
+
+} from "https://www.gstatic.com/firebasejs/10.9.0/firebase-auth.js";
+
+
+export const creatuserbySignUp = () => {
+
+
+    createUserWithEmailAndPassword(auth, email, passoword, username)
+
+        .then((userCredential) => {
+            const user = userCredential.user;
+        })
+
+        .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            // ..
+        })
+}
+
+
+export const userLogin = () => {
+    signInWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            const user = userCredential.user;
+        })
+        .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+        });
+}
+
+export const userLogOut = ( ) =>
+{
+
+    signOut(auth)
+    .then(() => {
+      window.location.href = "./login.html"
+    }).catch((error) => {
+
+        console.error(error)
+})}
+
+export const getCurrentUser = async()=>{
+try {
+    const user = auth.currentUser;
+    return await user;
+} catch (error) {
+    
+}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
